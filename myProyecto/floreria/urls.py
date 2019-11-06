8abe9302eece4104eb8a3e3f6738b09673587a24
GenesisVSS <gene.saez@alumnos.duoc.cl>
2019-11-06 00:06:56 -0300

    avance3

--- myProyecto/floreria/urls.py
+++ myProyecto/floreria/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import home
+from .views import home,galeria
 
 urlpatterns = [
     path('',home,name='home'),
+    path('galeria/',galeria,name='galeria'),
 ]
--- myProyecto/floreria/views.py
+++ myProyecto/floreria/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'core/home.html')
+def galeria(request):
+    return render(request,'core/galeria.html')
