1ccb2d1eead2211505e77df5a9c83d35541acb8c
GenesisVSS <gene.saez@alumnos.duoc.cl>
2019-11-07 22:54:33 -0300

    GaleriayFormulario

--- myProyecto/floreria/urls.py
+++ myProyecto/floreria/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import home,galeria
+from .views import home,galeria,formulario
 
 urlpatterns = [
     path('',home,name='home'),
     path('galeria/',galeria,name='galeria'),
+    path('formulario/',formulario,name='formulario'),
 ]
--- myProyecto/floreria/views.py
+++ myProyecto/floreria/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from .models import Flores 
 
 # Create your views here.
 def home(request):
     return render(request,'core/home.html')
 def galeria(request):
-    return render(request,'core/galeria.html')
+    flores=Flores.objects.all()
+    return render(request,'core/galeria.html',{'listaFlores':flores})
+def formulario(request):
+    florr=Flores.objects.all()# select * from Categoria
+    if request.POST:
+        Name=request.POST.get("txtNombre")
+        Valor=request.POST.get("txtValor")
+        Descripcion=request.POST.get("txtDesc")
+        Estado=request.POST.get("txtEstado")
+        Stock=request.POST.get("txtStock")
+        #recuperar la imagen desde el formulario
+        Imagen=request.FILES.get("txtImagen")
+        #crear una instancia de Pelicula (modelo)
+        flor=Flores(
+            name=Name,
+            valor=Valor,
+            descripcion=Descripcion,
+            estado=Estado,
+            stock=Stock,
+            imagen=Imagen
+        )
+        flor.save() #graba el objeto e bdd
+        return render(request,'core/formulario.html',{'lista':florr,'msg':'grabo','sw':True})
+    return render(request,'core/formulario.html',{'lista':florr})#pasan los datos a la web
