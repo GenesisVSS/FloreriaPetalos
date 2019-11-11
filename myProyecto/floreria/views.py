d346a343e417b4f7bff3cd2902df447f9bcfa246
GenesisVSS <gene.saez@alumnos.duoc.cl>
2019-11-11 00:26:33 -0300

    UsuariosLogin

--- myProyecto/floreria/views.py
+++ myProyecto/floreria/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Flores 
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,logout,login as auth_login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/principal_login/')
 def home(request):
     return render(request,'core/home.html')
+
 def galeria(request):
     flores=Flores.objects.all()
     return render(request,'core/galeria.html',{'listaFlores':flores})
@@ -34,6 +39,31 @@ def eliminar_flor(request,id):
     flor.delete()
         
     return HttpResponse("<script> ;window.location.href='/galeria/';</script>")
+
+def principal_login(request):
+    return render(request,'core/principal_login.html')
+
+def login_inicio(request):
+    if request.POST:
+        u=request.POST.get("txtUsuario")
+        c=request.POST.get("txtPassword")
+        usu=authenticate(request,username=u,password=c)
+        if usu is not None and usu.is_active:
+            auth_login(request, usu)
+            return render(request,'core/home.html')
+    return render(request,'core/principal_login.html')
+
+def login_admin(request):
+    return render(request,'core/login_admin.html')
+
+def login_usuario(request):
+    return render(request,'core/login_usuario.html')
+
+def cerrar_sesion(request):
+    logout(request)
+    return HttpResponse("<script>alert('cerro sesion');window.location.href='/';</script>")
+
+
         
 
 
